43faa9666c5d2fde128e97195dddef651858d6b1
Zeeshan Mughal <81948080+Batwings208@users.noreply.github.com>
2022-10-31 18:53:07 +0500

    Update ScreenLock.py

--- ScreenLock.py
+++ ScreenLock.py
@@ -2,6 +2,8 @@
 #For educational Purposes
 #Created By Batwings208
 #You are responsible for your actions
+#I expect a programmer to be using this as someone who doesn't wont understand the code, this is for you to figure out as its a potentially dangerous code
+# so I am not explaing it for that reason
 from tkinter import *
 from tkinter import ttk
 from pynput import *
@@ -21,10 +23,10 @@ tk = Tk()
 tk.attributes('-fullscreen', True)
 tk.resizable(False, False)
 
-label1 = ttk.Label(tk, text='Facial Recognition Required for further access', font='Futura 8')
+label1 = ttk.Label(tk, text='DO NOT USE INTENTIONS OF HARM', font='Futura 8')
 label1.place(relx=0.0, rely=0.0, anchor=NW)
 
-button1 = ttk.Button(tk, text='Begin Facial Recognition', command=tk.destroy)
+button1 = ttk.Button(tk, text='Begin', command=tk.destroy)
 button1.place(width=200, height=40, relx=0.5, rely=0.5, anchor=CENTER)
 
 mainloop()
